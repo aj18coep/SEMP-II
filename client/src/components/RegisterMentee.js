585a0c2829d8a00b605c69922a0069b2a55f05b0
avd151 <deshpandeav19.comp@coep.ac.in>
2022-03-25 14:10:34 +0530

    modified use case diagram

--- client/src/components/RegisterMentee.js
+++ client/src/components/RegisterMentee.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "../App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 export default function RegisterMentee() {
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [registerStatus, setRegisterStatus] = useState("");
+
+  const registerMentee = () => {
+    Axios.post("http://localhost:3002/signup-mentee", {
+      username: usernameReg,
+      password: passwordReg,
+    }).then((response) => {
+      setRegisterStatus(response.data);
+    });
+  };
   return (
-    <div>
-      <h1>This is RegisterMentee page</h1>
+    <div className="signup-mentee-form">
+      <h1>Sign Up</h1>
+      <label>Username</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }}
+      />
+      <label>Password</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}
+      />
+      <button onClick={registerMentee}>Register</button>
+      <h1>{registerStatus}</h1>
     </div>
   );
 }
