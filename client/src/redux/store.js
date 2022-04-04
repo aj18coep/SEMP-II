de9d0fdcfb8f106efc128a51a6c7ed9af351c488
avd151 <deshpandeav19.comp@coep.ac.in>
2022-04-04 22:37:40 +0530

    added combined root reducer for all redux components

---
+++ client/src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import adminReducer from "./Admin/adminReducer";
+import sessionReducer from "./Session/sessionReducer";
+import userReducer from "./Users/userReducer";
+
+const rootReducer = combineReducers({
+  users: userReducer,
+  session: sessionReducer,
+  admin: adminReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
