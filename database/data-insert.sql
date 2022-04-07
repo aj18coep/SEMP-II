f64bea8908843748ca532af7d9be1d739cdc1057
avd151 <deshpandeav19.comp@coep.ac.in>
2022-04-07 06:55:33 +0530

    added sql queries to insert data

---
+++ database/data-insert.sql
@@ -0,0 +1,5 @@
+insert into users(user_id, username, password) values (-1, "demo", "demo");
+
+insert into languages(language_name) values ("English"), ("Marathi"), ("Hindi"), ("Kannada"), ("Tamil"), ("Gujarati");
+
+insert into skills(skill_name) values ("Data Structures & Algorithms"), ("Web Development"), ("Machine Learning"), ("Deep Learning"), ("Data Science"), ("Software Engineering"), ("Android App Development"), ("Native App Development"), ("MERN Stack"), ("Software Engineering"), ("Operating Systems"), ("Database Management"), ("Computer Networks"), ("C"), ("C++"), ("Python"), ("Django"), ("ReactJS"), ("Blockchain");
