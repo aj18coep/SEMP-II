6eef2e7e1c6b867860f046fdcffa0a46753f2cb2
avd151 <deshpandeav19.comp@coep.ac.in>
2022-04-07 09:25:46 +0530

    added code to start the server

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const config = require("./config");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const routes = ["users", "session"];
+
+app.use(
+  cors({
+    origin: ["http://localhost:4000"], //3000
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    name: "cookie123",
+    key: "userId",
+    secret: "randomsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 24 * 60 * 60,
+    },
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send("Server Main page"));
+
+routes.forEach((route) => app.use(`/${route}`, require(`./routes/${route}`)));
+//app.use("/users", require("./routes/users"))
+app.listen(config.port, () => {
+  console.log(`Server listening on port no ${config.port}`);
+});
